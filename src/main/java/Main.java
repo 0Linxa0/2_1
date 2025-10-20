b4143b295544ee074c14779be21a4d1f0ea5f948
JINX <92461540+0Linxa0@users.noreply.github.com>
2025-10-20 07:09:51 +0000

    2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,12 @@
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!!!");
+    String tekst = "Hello World!!!";
+  for (int i = 0; i < tekst.length(); i++)
+    {
+      System.out.println(tekst.charAt(i));
+    }
+    
   }
 
 
